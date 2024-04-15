102e3a7f8d16fa3bbeec05e0972fdbea5b7de9ea
Saahil Shaikh <saahils191@gmail.com>
2024-04-15 09:59:51 +0530

    Fixed authcontroller

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -7,7 +7,7 @@ const handleLogin = async(req,res) =>{
     if(!user || !pwd) return res.status(400).json({'message':'Username and Password are Neecessary!'});
 
     const foundUser = await User.findOne({username: user}).exec();
-    if(!foundUser) return res.sendStaus(401) //Unauthorised
+    if(!foundUser) return res.sendStatus(401) //Unauthorised
 
     //Evaluation of password
     const match = await bcrypt.compare(pwd,foundUser.password);
@@ -32,7 +32,7 @@ const handleLogin = async(req,res) =>{
         const result = await foundUser.save();
         console.log(result);
 
-        res.cookie('jwt',refreshToken, {httpOnly:true, samesite:None,maxage: 24 * 60*60*1000});
+        res.cookie('jwt',refreshToken, {httpOnly:true, samesite:'None',maxage: 24 * 60*60*1000});
         res.json({ accessToken }); //fetch this in the client
     }else{
         res.sendStatus(401);
