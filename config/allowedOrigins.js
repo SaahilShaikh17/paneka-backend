bbeb305464ec93f6487576813a54b6a912cca1db
Saahil Shaikh <saahils191@gmail.com>
2024-04-15 10:48:57 +0530

    added origin for client

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:1337',
-    'https://panenka-two.vercel.app/'
+    'https://panenka-two.vercel.app/',
+    'http://localhost:3000/'
 ];
 
 module.exports = allowedOrigins;
