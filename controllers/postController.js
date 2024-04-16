feea6896aa78460293fcf1b17b64840f06b077b3
Saahil Shaikh <saahils191@gmail.com>
2024-04-16 08:44:28 +0530

    Lol console.log

--- controllers/postController.js
+++ controllers/postController.js
@@ -6,8 +6,8 @@ const getAllposts = async (req,res) => {
     try {
         const posts = await Post.find().populate('author','username');
         if(!posts) return res.status(204).json({'message':'No posts found'});
-        console.log(posts);
-        // res.json(posts)
+        // console.log(posts);
+         res.json(posts)
     }catch(err){
         res.status(500).json({message: err.message});
     }
